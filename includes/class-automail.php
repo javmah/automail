e85c82e4307917dba1bf17466f5c03b3e780fc2f
DESKTOP-QEP0TND\Javmah <jaedmah@gmail.com>
2021-09-29 20:15:05 +0600

    ref-8

--- includes/class-automail.php
+++ includes/class-automail.php
@@ -1,25 +1,9 @@
 <?php
 /**
  * The file that defines the core plugin class
- * A class definition that includes attributes and functions used across both the
- * public-facing side of the site and the admin area.
- *
- * @link       https://profiles.wordpress.org/javmah/
- * @since      1.0.0
- *
- * @package    Automail
- * @subpackage Automail/includes
- */
-
-/**
- * The core plugin class.
- *
- * This is used to define internationalization, admin-specific hooks, and
- * public-facing site hooks.
- *
- * Also maintains the unique identifier of this plugin as well as the current
- * version of the plugin.
- *
+ * A class definition that includes attributes and functions used across both the public-facing side of the site and the admin area.
+ * This is used to define internationalization, admin-specific hooks, and public-facing site hooks.
+ * Also maintains the unique identifier of this plugin as well as the current version of the plugin.
  * @since      1.0.0
  * @package    Automail
  * @subpackage Automail/includes
@@ -28,12 +12,11 @@
 class Automail {
 
 	/**
-	 * The loader that's responsible for maintaining and registering all hooks that power
-	 * the plugin.
+	 * The loader that's responsible for maintaining and registering all hooks that power the plugin.
 	 * @since    1.0.0
 	 * @access   protected
 	 * @var      Automail_Loader    $loader    Maintains and registers all hooks for the plugin.
-	 */
+	*/
 	protected $loader;
 
 	/**
@@ -41,7 +24,7 @@ class Automail {
 	 * @since    1.0.0
 	 * @access   protected
 	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
-	 */
+	*/
 	protected $plugin_name;
 
 	/**
@@ -49,16 +32,14 @@ class Automail {
 	 * @since    1.0.0
 	 * @access   protected
 	 * @var      string    $version    The current version of the plugin.
-	 */
+	*/
 	protected $version;
 
 	/**
-	 * Define the core functionality of the plugin.
-	 * Set the plugin name and the plugin version that can be used throughout the plugin.
-	 * Load the dependencies, define the locale, and set the hooks for the admin area and
-	 * the public-facing side of the site.
+	 * Define the core functionality of the plugin. Set the plugin name and the plugin version that can  be used throughout the plugin. 
+	 * Load the dependencies, define the locale, and set the hooks for the admin area and the public-facing side of the site.
 	 * @since    1.0.0
-	 */
+	*/
 	public function __construct() {
 		if ( defined( 'AUTOMAIL_VERSION' ) ) {
 			$this->version = AUTOMAIL_VERSION;
@@ -71,12 +52,10 @@ class Automail {
 		$this->set_locale();
 		$this->define_admin_hooks();
 		$this->define_public_hooks();
-
 	}
 
 	/**
 	 * Load the required dependencies for this plugin.
-	 *
 	 * Include the following files that make up the plugin:
 	 *
 	 * - Automail_Loader. Orchestrates the hooks of the plugin.
@@ -84,60 +63,40 @@ class Automail {
 	 * - Automail_Admin. Defines all hooks for the admin area.
 	 * - Automail_Public. Defines all hooks for the public side of the site.
 	 *
-	 * Create an instance of the loader which will be used to register the hooks
-	 * with WordPress.
+	 * Create an instance of the loader which will be used to register the hooks with WordPress.
 	 * @since    1.0.0
 	 * @access   private
-	 */
+	*/
 	private function load_dependencies() {
-
-		/**
-		 * The class responsible for orchestrating the actions and filters of the
-		 * core plugin.
-		*/
+		# The class responsible for orchestrating the actions and filters of the core plugin.
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-automail-loader.php';
-
-		/**
-		 * The class responsible for defining internationalization functionality
-		 * of the plugin.
-		*/
+		# The class responsible for defining internationalization functionality of the plugin.
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-automail-i18n.php';
-
-		/**
-		 * The class responsible for defining all actions that occur in the admin area.
-		*/
+		# The class responsible for defining all actions that occur in the admin area.
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-automail-admin.php';
-
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		*/
+		# The class responsible for defining all actions that occur in the public-facing side of the site.
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-automail-public.php';
-
+		# 
 		$this->loader = new Automail_Loader();
 	}
 
 	/**
 	 * Define the locale for this plugin for internationalization.
-	 * Uses the Automail_i18n class in order to set the domain and to register the hook
-	 * with WordPress.
+	 * Uses the Automail_i18n class in order to set the domain and to register the hook with WordPress.
 	 * @since    1.0.0
 	 * @access   private
-	 */
+	*/
 	private function set_locale() {
 
 		$plugin_i18n = new Automail_i18n();
-
 		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
 	}
 
 	/**
-	 * Register all of the hooks related to the admin area functionality
-	 * of the plugin.
-	 *
+	 * Register all of the hooks related to the admin area functionality of the plugin.
 	 * @since    1.0.0
 	 * @access   private
-	 */
+	*/
 	private function define_admin_hooks() {
 
 		$plugin_admin = new Automail_Admin( $this->get_plugin_name(), $this->get_version() );
@@ -152,9 +111,7 @@ class Automail {
 	}
 
 	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
+	 * Register all of the hooks related to the public-facing functionality of the plugin.
 	 * @since    1.0.0
 	 * @access   private
 	 */
@@ -168,40 +125,35 @@ class Automail {
 
 	/**
 	 * Run the loader to execute all of the hooks with WordPress.
-	 *
 	 * @since    1.0.0
-	 */
+	*/
 	public function run() {
 		$this->loader->run();
 	}
 
 	/**
-	 * The name of the plugin used to uniquely identify it within the context of
-	 * WordPress and to define internationalization functionality.
-	 *
+	 * The name of the plugin used to uniquely identify it within the context of WordPress and to define internationalization functionality.
 	 * @since     1.0.0
 	 * @return    string    The name of the plugin.
-	 */
+	*/
 	public function get_plugin_name() {
 		return $this->plugin_name;
 	}
 
 	/**
 	 * The reference to the class that orchestrates the hooks with the plugin.
-	 *
 	 * @since     1.0.0
 	 * @return    Automail_Loader    Orchestrates the hooks of the plugin.
-	 */
+	*/
 	public function get_loader() {
 		return $this->loader;
 	}
 
 	/**
 	 * Retrieve the version number of the plugin.
-	 *
 	 * @since     1.0.0
 	 * @return    string    The version number of the plugin.
-	 */
+	*/
 	public function get_version() {
 		return $this->version;
 	}
